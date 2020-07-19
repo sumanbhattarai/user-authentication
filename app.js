78b9f4d471871ef875d590cd0e4875445326c415
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-07-19 20:00:09 +0545

    updated

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const auth  = require('./routes/auth')
 const port = process.env.PORT || 3000
+require('dotenv').config()
 
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-
-
 app.use('/api/user' , auth )
+
+mongoose.connect(
+    process.env.DB_URl,
+    { useNewUrlParser: true , useUnifiedTopology: true },
+    ()=>{console.log('Connected to DB.')}
+)
 
 app.listen(port , ()=>{
     console.log(`Server is running at ${port}.`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,47 @@
 const express = require('express')
+const bycrypt = require('bcryptjs')
 const router = express.Router()
+const User  = require('../models/User')
 
-router.post('/register' , (req , res)=>{
-    res.send('Register')
+router.post('/register' , async (req , res)=>{
+    //check if email already exists
+    const emailExist  = await User.findOne({email  :req.body.email})
+    // if emailExist is true then
+    if(emailExist){
+        return res.status(400).send('Email already Exists.')
+    }
+
+    //hashing password
+    const salt =  await bycrypt.genSalt(10)
+    const hashedPassword = await bycrypt.hash(req.body.password , salt)
+
+    const user = new User({
+        email : req.body.email,
+        password : hashedPassword
+    })
+    try {
+        await user.save()
+        res.status(200).send({ user : user._id})
+    }catch(error){
+        res.status(400).send(error)
+    }
 })
 
-router.post('/login' , (req , res)=>{
-    res.send('Login')
+router.post('/login' , async (req , res)=>{
+     //check if email exists
+     const user  = await User.findOne({email  :req.body.email})
+     if(!user){
+        return res.status(400).send('Invalid email or password.')
+     }
+
+     const validPass = await bycrypt.compare(req.body.password , user.password)
+     if(validPass){
+         res.status(200).send('Logged In.')
+     } else {
+         res.status(400).send('Invalid email or password.')
+     }
+
+
 })
 
 module.exports = router
